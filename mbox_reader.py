49cf81b4b460e7f168cd243ed0ea2fe9e3b4b832
Derrick Ishman <twohackerauth@proton.me>
2026-04-29 16:06:12 -0500

    adding logging functionality to script

--- mbox_reader.py
+++ mbox_reader.py
@@ -1,13 +1,35 @@
 #!/home/derrick/venv/bin/python3
 
+from datetime import datetime as dt
 from re import search
 from time import sleep
+import logging
 
 from mailbox import mbox
 from os import path
 from subprocess import run
 
 
+# LOGGING CONFIGURATION
+formatter = logging.Formatter(fmt="%(levelname)s %(asctime)s: %(message)s (Line: %(lineno)d in %(filename)s)",
+                              datefmt="%H:%M:%S")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler(f"copyright_{dt.now().strftime("%m-%d-%y_%H:%M:%S")}.log", "w")
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+# Set global variables
 MAILDIR = '/home/derrick/.thunderbird/28rm5iqs.default-release/Mail/Local Folders/Copyright'
 TEMPDIR = '/home/derrick/.local/share/derrick/default/projects/copyright_emails/'
 
@@ -34,41 +56,35 @@ def parse_ip(filename):
                 return match.group()
 
 
+def parse_sub(response: str):
+    e9 = search(r'[A-Z][a-z\-]{2,11}-E9-1', response)
+    ont_id = search(r'\(\d{2,5}', response)
+    return e9.group(), ont_id.group().lstrip('(')
+
+
 def cleanup():
     rm = run('rm *.xml', shell=True)
     return rm
 
 
 def locate_customer(address: str):
-    # from juniper.routers import routers
-    # from juniper.mx import MX
     from calix.cx_detail import cx
     from calix.smx_search import search_all
 
-    query = search_all(address)
-
-    # for ip, hostname in routers.items():
-    #    junos = MX(ip, hostname)
-    #    print(f"Checking {hostname}...")
-    #    binding = junos.connection.send_command_timing(
-    #        f"show dhcp relay binding {address} detail")
-    #    print(binding)
-    #    if "BOUND" in binding:
-    #        print(f"Customer located on {hostname}")
-    #        circuit_id = junos.connection.send_command_timing(f"show subscribers detail address {address} | match Circuit")
-    #        if circuit_id == '':
-    #            print(f"No circuit id listed for address {address}")
-    #            continue
-    #        e9 = circuit_id.split(':')[1].lstrip()
-    #        ont_id = search(r'\d{3,5}$', circuit_id.split(':')[2].rstrip('\n'))
-    #        customer = cx(e9, ont_id.group())
-    #        if customer is not None:
-    #            em = customer.get('locations')[0].get('contacts')[0].get('email', "No email").lower()
-    #            print(f"{address}, {em}")
-    #            return em
-    #        else:
-    #            print(f"No customer information available for address {address}")
-    #            return
+    response = search_all(address)
+    if response.status_code == 200:
+        data = response.json()[0].get('desc')
+    e9 = search(r'[A-Z][a-z\-]{2,11}-E9-1', data)
+    ont_id = search(r'\(\d{2,5}', data)
+    customer = cx(e9.group(), ont_id.group().lstrip('('))
+    if customer is not None:
+        em = customer.get('locations')[0].get('contacts')[0].get('email', "No email").lower()
+        print(f"{address}, {em}")
+        sleep(1)
+        return em
+    else:
+        print(f"No customer information available for address {address}")
+        return
 
 
 def parse_messages():
@@ -83,7 +99,6 @@ def parse_messages():
                 sleep(1)
                 addresses.append(address)
                 attachments.append(file)
-    addresses = list(dict.fromkeys(addresses))
     return addresses, attachments
 
 
@@ -94,13 +109,21 @@ def get_emails(addresses: list) -> list:
 
 def compose_email(emails: list, attachments: list):
     from copyright_body import body
-    # for email, attachment in zip(emails, attachments):
-    #    thunderbird = run("thunderbird -compose \"to=\'dishman@cvecfiber.com\',subject=\'Notice of Claimed Infringement\',body={body},attacthment={attachment})
-    thunderbird = run(['thunderbird', '-compose', f"to='dishman@cvecfiber.com',subject='Notice of Claimed Infringement',body={body}"])
+    for email, attachment in zip(emails, attachments):
+        run(['thunderbird', '-compose', f"to={email},subject='Notice of Claimed Infringement',body={body},attachment={attachment}"])
 
 
 if __name__ == "__main__":
-    # addresses, attachments = parse_messages()
-    # emails = get_emails(addresses)
-    compose_email(1, 2)
-    # cleanup()
+    addresses, attachments = parse_messages()
+    emails = get_emails(addresses)
+    for email, attachment in zip(emails, attachments):
+        print(f"\n{email}, {attachment.split('/')[9]}")
+        sleep(1)
+    compose_email(emails, attachments)
+    print(f'\nSleeping for {len(addresses)} seconds')
+    sleep(len(addresses))
+    send_emails = input("Done sending mail?: ")
+    if send_emails == 'y':
+        print('Cleaning up...')
+        cleanup()
+    print('Done')
