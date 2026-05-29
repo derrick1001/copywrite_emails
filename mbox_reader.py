926621ac64969d6d6b64649b8595ad7344f6c9b8
Derrick Ishman <twohackerauth@proton.me>
2026-05-29 09:04:25 -0500

    changed the way we clean the MAILDIR file from echo "" to just >

--- mbox_reader.py
+++ mbox_reader.py
@@ -66,7 +66,7 @@ def cleanup():
     logger.info(f"Removing all .xml files from {WORKDIR}")
     run('rm *.xml', shell=True)
     logger.info(f"Removing parsed messages from {MAILDIR}")
-    run(f'echo "" > {MAILDIR}', shell=True)
+    run(f'> {MAILDIR}', shell=True)
 
 
 def locate_customer(address: str):
