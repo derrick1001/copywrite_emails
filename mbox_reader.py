9af2219520f792cf5f902823d1eaaec8c5968b2f
Derrick Ishman <twohackerauth@proton.me>
2026-05-29 10:24:46 -0500

    again

--- mbox_reader.py
+++ mbox_reader.py
@@ -66,7 +66,7 @@ def cleanup():
     logger.info(f"Removing all .xml files from {WORKDIR}")
     run('rm *.xml', shell=True)
     logger.info(f"Removing parsed messages from {MAILDIR}")
-    run("cat /dev/null", stdout=f"{MAILDIR}")
+    run("cat /dev/null", stdout=MAILDIR)
 
 
 def locate_customer(address: str):
