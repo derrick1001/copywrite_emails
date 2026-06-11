3dfe01edcd03ed04e3bf5b3c52ac821ecd0ff0d4
Derrick Ishman <twohackerauth@proton.me>
2026-06-11 09:22:16 -0500

    removed printing the list of emails from script

--- counter.py
+++ counter.py
@@ -2,8 +2,6 @@ counts = {}
 with open('offenders/tracked.txt', 'r') as f:
     emails = [email.rstrip('\n') for email in f]
 
-print(f'Emails: {emails}\n')
-
 for email in emails:
     counts[email] = counts.get(email, 0) + 1
 
